b3b9aa4e003b23f3abcdf86879aad636600e338a
jesse-perkins <91579971+jesse-perkins@users.noreply.github.com>
2021-09-29 20:09:17 -0600

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace PrimalityTester
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.kValue = new System.Windows.Forms.TextBox();
+            this.solve = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(138, 49);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(532, 22);
+            this.input.TabIndex = 0;
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(138, 274);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(532, 22);
+            this.output.TabIndex = 3;
+            // 
+            // kValue
+            // 
+            this.kValue.Location = new System.Drawing.Point(138, 104);
+            this.kValue.Name = "kValue";
+            this.kValue.Size = new System.Drawing.Size(247, 22);
+            this.kValue.TabIndex = 1;
+            // 
+            // solve
+            // 
+            this.solve.Location = new System.Drawing.Point(517, 169);
+            this.solve.Name = "solve";
+            this.solve.Size = new System.Drawing.Size(153, 65);
+            this.solve.TabIndex = 2;
+            this.solve.Text = "SOLVE";
+            this.solve.UseVisualStyleBackColor = true;
+            this.solve.Click += new System.EventHandler(this.solve_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "INPUT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "k VALUE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "OUTPUT";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 336);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.solve);
+            this.Controls.Add(this.kValue);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Name = "Form2";
+            this.Text = "PRIMALITY TESTER";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.TextBox kValue;
+        private System.Windows.Forms.Button solve;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimalityTester
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void solve_Click(object sender, EventArgs e)
+        {
+            
+            Int64 N = Convert.ToInt64(input.Text);
+            Int64 k = Convert.ToInt64(kValue.Text);
+            if (!(k < N))
+            {
+                output.Text = "ERROR. k value must be less than input.";
+            }
+            else if (Little_Fermi(N, k))
+            {
+                double prob = 1 - 1 / Math.Pow(2, k);
+                output.Text = "Yes, with probability " + prob;
+            }
+            else
+            {
+                output.Text = "No.";
+            }
+            return;
+        }
+
+        private bool Little_Fermi(Int64 N, Int64 k)
+        {
+            Random rnd = new Random();
+            HashSet<Int64> randomNums = new HashSet<Int64>();
+            while (randomNums.Count() < k)
+            {
+                randomNums.Add(rnd.Next(1, (Int32)N));
+            }
+            foreach (Int64 num in randomNums)
+            {
+                if (Modular_Exponentiation(num, N - 1, N) != 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private Int64 Modular_Exponentiation(Int64 x, Int64 y, Int64 N)
+        {
+            if (y == 0) { return 1; }
+            double yHalved = y / 2;
+            Int64 z = Modular_Exponentiation(x, (int)Math.Floor(yHalved), N);
+            if (y % 2 == 0)
+            {
+                Int64 zSquared = (z % N) * (z % N);
+                zSquared = zSquared % N;
+                return (Int64)(zSquared);
+            }
+            else
+            {
+                Int64 zSquaredx = (((z % N) * (z % N)) % N) * (x % N);
+                zSquaredx = zSquaredx % N;
+                return (Int64)(zSquaredx);
+            }
+        }
+    }
+}
